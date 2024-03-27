e96e2ae9482dc4adcaaeff96ef1853c31cc40219
S5200018 <52000128@student.tdtu.edu.vn>
2024-03-27 20:24:30 +0700

    Done b3

--- FE/freshmarket/src/index.js
+++ FE/freshmarket/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
+import { BrowserRouter } from 'react-router-dom';
+import RoutersCustom from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Fresh Market</h1>
+  <BrowserRouter>
+    <RoutersCustom/>
+  
+  </BrowserRouter>
 );
---
+++ FE/freshmarket/src/router.js
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/users/homePage";
+import { ROUTERS } from "./utils/router";
+import MasterLayout from "./pages/users/theme/masterLayout";
+import ProfilePage from "./pages/users/profilePage";
+
+const renderUserRouter = () => {
+    const userRouters = [
+        {
+            path: ROUTERS.USER.HOME,
+            component: <HomePage/>
+        },
+        {
+            path: ROUTERS.USER.PROFILE,
+            component: <ProfilePage/>
+        }
+    ]
+
+    return (
+        <MasterLayout>
+            <Routes>
+                {
+                    userRouters.map((item, key) => (
+                        <Route key={key} path={item.path} element={item.component} />
+                    ))
+                }
+            </Routes>
+        </MasterLayout>
+    )
+}
+
+const RoutersCustom = () => {
+    return renderUserRouter();
+};
+export default RoutersCustom;
