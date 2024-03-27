fde2c1cdfca174db8419548960a9ad9c047588d6
S5200018 <52000128@student.tdtu.edu.vn>
2024-03-27 23:34:34 +0700

    header

--- FE/freshmarket/src/pages/users/theme/header/index.js
+++ FE/freshmarket/src/pages/users/theme/header/index.js
@@ -1,7 +1,146 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import "./style.scss"
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { formatter } from "utils/formatter";
+import { FaShoppingCart } from "react-icons/fa";
+import { ROUTERS } from "utils/router"
+
 
 const Header = () => {
-    return <h1>Header</h1>
+    const [menus, setMenu] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Cửa hàng",
+            path: ROUTERS.USER.SHOP,
+        },
+        {
+            name: "Sản phẩm",
+            path: ROUTERS.USER.PRODUCTS,
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Thịt",
+                    path: ROUTERS.USER.MEAL,
+                },
+                {
+                    name: "Rau củ",
+                    path: ROUTERS.USER.VEGETABLES,
+                },
+                {
+                    name: "Thức ăn nhanh",
+                    path: ROUTERS.USER.FASTFOOD,
+                },
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: ROUTERS.USER.POSTS,
+        },
+        {
+            name: "Liên hệ",
+            path: ROUTERS.USER.CONTACT,
+        },
+    ])
+    return (
+        <>
+            <div className="header-top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header-top-left">
+                            <ul>
+                                <li>
+                                    <IoMail />hello@gmail.com
+                                </li>
+                                <li>
+                                    Miễn phí ship đơn hàng từ  {formatter(200000)}
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header-top-right">
+                            <ul>
+                                <li>
+                                    <Link to={""}>
+                                        <FaFacebook/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FaSquareInstagram />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FaTwitter />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FaLinkedin />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FaUser />
+                                    </Link>
+                                    <span>Đăng nhập</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3 col-lg-3">
+                        <div className="header-logo">
+                            <h2>FRESH MARKET</h2>
+                        </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6">
+                        <nav className="header-menu">
+                            <ul>
+                                {
+                                    menus?.map((menu, menuKey) => (
+                                        <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                                            <Link to={menu?.path}>{menu?.name}</Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3 col-lg-3">
+                        <div className="header-cart">
+                            <div className="header-cart-price">
+                                <span>{formatter(176000)}</span>
+                            </div>   
+                            <ul>
+                                <li>
+                                    <Link to="#">
+                                        <FaShoppingCart />
+                                        <span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+        
+        
+
+    )
 };
 
 export default memo(Header);
--- FE/freshmarket/src/utils/router.js
+++ FE/freshmarket/src/utils/router.js
@@ -2,5 +2,9 @@ export const ROUTERS = {
     USER: {
         HOME: "",
         PROFILE: "profile",
+        SHOP: "cua-hang",
+        PRODUCTS: "san-pham",
+        POSTS: "bai-viet",
+        CONTACT: "lien-he"
     }
 }
